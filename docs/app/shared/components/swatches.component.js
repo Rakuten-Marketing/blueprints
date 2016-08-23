5c736bf567f199a097ce032e4a5ae6de36a3efed
Angel Samano <angel.samano@mail.rakuten.com>
2016-08-23 16:06:18 -0400

    RAN-863: Separating color palettes

--- docs/app/shared/components/swatches.component.js
+++ docs/app/shared/components/swatches.component.js
@@ -1,25 +1,30 @@
-angular.module('app')
+(function() {
+  'use strict';
+  angular.module('app').component('swatches', {
+    bindings: {
+      palette: '@palette'
+    },
 
-  .component('swatches', {
-    templateUrl: '/swatches.component.html',
-    controller: ColorSwatchesController
-  })
+    templateUrl: 'app/shared/components/swatches.component.html',
+    controller: function($scope, $http) {
+      var vm = this;
 
-  function ColorSwatchesController($scope, $http) {
-    var vm = this;
-    
-    vm.colors = {};
-		$http.get('build/docs/variables.json').success(function (response) {
-			var processed = [],
-					data = response['colors'];
+      vm.colors = {};
+      $http.get('build/docs/variables.json').success(function (response) {
+        var processed = [],
+        data = response['colors'];
 
-			angular.forEach(data, function(value, key){
-				processed.push({
-					id: key,
-					value: value
-				});
-			});
+        angular.forEach(data, function(value, key){
+          if (key.indexOf(vm.palette) !== -1 || !vm.palette) {
+            processed.push({
+              id: key,
+              value: value
+            });
+          }
+        });
 
-			vm.colors = processed;
-		});
-  }
+        vm.colors = processed;
+      });
+    }
+  });
+})();
