a92ee97f16270ef32ca785f24cf58f6720aa43df
Angel Samano <angel.samano@mail.rakuten.com>
2016-08-24 12:40:04 -0400

    RAN-865: Move the Bootstrap "CSS" components to their own states (angular)

--- docs/app/partials/components/components.controller.js
+++ docs/app/partials/components/components.controller.js
@@ -2,9 +2,14 @@
   'use strict';
 
   angular.module('app').controller('ComponentsController', ComponentsController);
-  ComponentsController.$inject = ['$state'];
+  ComponentsController.$inject = ['$rootScope', '$state'];
 
-  function ComponentsController($state) {
+  function ComponentsController($rootScope, $state) {
     var vm = this;
+
+    vm.$state = $rootScope.$state;
+    vm.switchState = function(state) {
+      $state.go('application.components.' + state);
+    };
   }
 })();
