33043d28544a601e9f19e9f155071dd1803c0d43
Jon Swanson <jon.swanson@rakuten.com>
2017-01-23 02:30:02 -0800

    stopped rearranging ng bindings (#71)

--- src/core/webcomponents/dropdowns.js
+++ src/core/webcomponents/dropdowns.js
@@ -13,6 +13,18 @@
       this.removeAttribute("example");
       return;
     }
+    this.prepContent();
+    this.classList.add('dropdown');
+    this.animating = false;
+    this.open = false;
+  };
+
+  dropdownProto.prepContent = function () {
+    if (this.getElementsByClassName('dropdown-toggle').length) {
+      this.button = this.getElementsByClassName('dropdown-toggle')[0];
+      this.list = this.children[0];
+      return;
+    }
     var temp = document.createElement('DIV');
     this.list = this.children[0];
     this.list.classList.add('dropdown-menu');
@@ -32,10 +44,7 @@
       this.button.setAttribute('id', id);
       this.list.setAttribute('aria-labelledby', id);
     }
-    this.insertBefore(this.button, this.list);
-    this.classList.add('dropdown');
-    this.animating = false;
-    this.open = false;
+    this.appendChild(this.button);
   };
     
   dropdownProto.attachedCallback = function () {
